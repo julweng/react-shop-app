1668df6dbd9a99f73369ef18edb67befc08a8026
julweng <julweng@gmail.com>
2020-06-17 22:47:24 -0700

    prettier format

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
-import LeftMenu from './Sections/LeftMenu';
-import RightMenu from './Sections/RightMenu';
-import { Drawer, Button } from 'antd';
-import { AlignRightOutlined } from '@ant-design/icons';
-import './Sections/Navbar.css';
+import React, { useState } from "react"
+import LeftMenu from "./Sections/LeftMenu"
+import RightMenu from "./Sections/RightMenu"
+import { Drawer, Button } from "antd"
+import { AlignRightOutlined } from "@ant-design/icons"
+import "./Sections/Navbar.css"
 
 function NavBar() {
   const [visible, setVisible] = useState(false)
 
   const showDrawer = () => {
     setVisible(true)
-  };
+  }
 
   const onClose = () => {
     setVisible(false)
-  };
+  }
 
   return (
-    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
+    <nav
+      className="menu"
+      style={{ position: "fixed", zIndex: 5, width: "100%" }}
+    >
       <div className="menu__logo">
         <a href="/">Logo</a>
       </div>
